adceea17379272dfbf044afca124174a1e4f4dda
ryan-zayne <phoenixastro97@gmail.com>
2024-10-06 19:09:47 +0100

    final finals

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,62 +1,76 @@
 const Services = () => {
-    return ( 
-        <div className="my-40">
-            <h1 className="text-center font-bold text-4xl">Our Service</h1>
+	return (
+		<div className="my-40">
+			<h1 className="text-center text-4xl font-bold">Our Service</h1>
 
-            <div className="mt-8 grid items-center sm:grid sm:px-12 md:grid-cols-2 md:items-center lg:grid-cols-3 sxl:grid-cols-3 xl:grid-cols-3 gap-10 lg:gap-12 px-16 ">
+			<div
+				className="mt-8 grid items-center gap-10 px-16 md:grid-cols-2 md:items-center lg:grid-cols-3
+					lg:gap-12 sxl:grid-cols-3 xl:grid-cols-3"
+			>
+				<div
+					className="flex h-[30vh] w-full items-center rounded-lg border-b-8 border-borderBottom3
+						bg-cosWhite px-4 font-bold text-black shadow-2xl"
+				>
+					<div className="flex h-12 w-12 items-center justify-center rounded-full bg-slitePlink">
+						<i className="ri-school-line text-[36px] md:text-[44px]"></i>
+					</div>
+					<p className="pl-2 text-[20px] md:text-[18px] lg:text-[21px]">School Management System</p>
+				</div>
 
-            <div className="sm:w-[100%] sm:h-[35vh] md:w-full md:h-[30vh] rounded-lg px-4 bg-cosWhite text-black flex items-center font-bold shadow-2xl border-b-8 border-borderBottom3"> 
-                <div className="rounded-full w-12 h-12 bg-slitePlink flex justify-center items-center">
-                <i className="ri-school-line sm:text-[36px] md:text-[44px]"></i>
-                </div>
-                <p className="pl-2 sm:text-[20px] md:text-[18px] lg:text-[21px]">School Management System</p>
+				<div
+					className="flex h-[35vh] w-[100%] items-center rounded-lg border-b-8 border-cosBorder
+						bg-cosWhite px-4 font-bold text-black shadow-2xl md:h-[30vh] md:w-full"
+				>
+					<div className="flex h-12 w-12 items-center justify-center rounded-full bg-slitePlink">
+						<i className="ri-school-line text-[36px] md:text-[44px]"></i>
+					</div>
+					<p className="pl-2 text-[20px] md:text-[18px] lg:text-[21px]">Online Result Checker</p>
+				</div>
 
-            </div>
+				<div
+					className="flex h-[35vh] w-[100%] items-center rounded-lg border-b-8 border-borderBottom3
+						bg-cosWhite px-4 font-bold text-black shadow-2xl md:h-[30vh] md:w-full"
+				>
+					<div className="flex h-12 w-12 items-center justify-center rounded-full bg-slitePlink">
+						<i className="ri-school-line text-[36px] md:text-[44px]"></i>
+					</div>
+					<p className="pl-2 text-[20px] md:text-[18px] lg:text-[21px]">
+						Professional School Website
+					</p>
+				</div>
 
-            <div className="sm:w-[100%] sm:h-[35vh] md:w-full md:h-[30vh] rounded-lg px-4 bg-cosWhite text-black flex items-center font-bold shadow-2xl border-b-8 border-cosBorder"> 
-                <div className="rounded-full w-12 h-12 bg-slitePlink flex justify-center items-center">
-                <i className="ri-school-line sm:text-[36px] md:text-[44px]"></i>
-                </div>
-                <p className="pl-2 sm:text-[20px] md:text-[18px] lg:text-[21px]">Online Result Checker</p>
+				<div
+					className="flex h-[35vh] w-[100%] items-center rounded-lg border-b-8 border-borderBottom2
+						bg-cosWhite px-4 font-bold text-black shadow-2xl md:h-[30vh] md:w-full"
+				>
+					<div className="flex h-12 w-12 items-center justify-center rounded-full bg-slitePlink">
+						<i className="ri-school-line text-[36px] md:text-[44px]"></i>
+					</div>
+					<p className="pl-2 text-[20px] md:text-[18px] lg:text-[21px]">School Management System</p>
+				</div>
 
-            </div>
+				<div
+					className="flex h-[35vh] w-[100%] items-center rounded-lg border-b-8 border-borderBottom2
+						bg-cosWhite px-4 font-bold text-black shadow-2xl md:h-[30vh] md:w-full"
+				>
+					<div className="flex h-12 w-12 items-center justify-center rounded-full bg-slitePlink">
+						<i className="ri-school-line text-[36px] md:text-[44px]"></i>
+					</div>
+					<p className="pl-2 text-[20px] md:text-[18px] lg:text-[21px]">School Management System</p>
+				</div>
 
-            <div className="sm:w-[100%] sm:h-[35vh] md:w-full md:h-[30vh] rounded-lg px-4 bg-cosWhite text-black flex items-center font-bold shadow-2xl border-b-8 border-borderBottom3"> 
-                <div className="rounded-full w-12 h-12 bg-slitePlink flex justify-center items-center">
-                <i className="ri-school-line sm:text-[36px] md:text-[44px]"></i>
-                </div>
-                <p className="pl-2 sm:text-[20px] md:text-[18px] lg:text-[21px]">Professional School
-                Website</p>
+				<div
+					className="flex h-[35vh] w-[100%] items-center rounded-lg border-b-8 border-borderBottom3
+						bg-cosWhite px-4 font-bold text-black shadow-2xl md:h-[30vh] md:w-full"
+				>
+					<div className="flex h-12 w-12 items-center justify-center rounded-full bg-slitePlink">
+						<i className="ri-school-line text-[36px] md:text-[44px]"></i>
+					</div>
+					<p className="pl-2 text-[20px] md:text-[18px] lg:text-[21px]">School Management System</p>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-            </div>
-
-            <div className="sm:w-[100%] sm:h-[35vh] md:w-full md:h-[30vh] rounded-lg px-4 bg-cosWhite text-black flex items-center font-bold shadow-2xl border-b-8 border-borderBottom2"> 
-                <div className="rounded-full w-12 h-12 bg-slitePlink flex justify-center items-center">
-                <i className="ri-school-line sm:text-[36px] md:text-[44px]"></i>
-                </div>
-                <p className="pl-2 sm:text-[20px] md:text-[18px] lg:text-[21px]">School Management System</p>
-
-            </div>
-
-            <div className="sm:w-[100%] sm:h-[35vh] md:w-full md:h-[30vh] rounded-lg px-4 bg-cosWhite text-black flex items-center font-bold shadow-2xl border-b-8 border-borderBottom2"> 
-                <div className="rounded-full w-12 h-12 bg-slitePlink flex justify-center items-center">
-                <i className="ri-school-line sm:text-[36px] md:text-[44px]"></i>
-                </div>
-                <p className="pl-2 sm:text-[20px] md:text-[18px] lg:text-[21px]">School Management System</p>
-            </div>
-
-            <div className="sm:w-[100%] sm:h-[35vh] md:w-full md:h-[30vh] rounded-lg px-4 bg-cosWhite text-black flex items-center font-bold shadow-2xl border-b-8 border-borderBottom3"> 
-                <div className="rounded-full w-12 h-12 bg-slitePlink flex justify-center items-center">
-                <i className="ri-school-line sm:text-[36px] md:text-[44px]"></i>
-                </div>
-                <p className="pl-2 sm:text-[20px] md:text-[18px] lg:text-[21px]">School Management System</p>
-
-            </div>
-
-            </div>
-            
-        </div>
-     );
-}
- 
 export default Services;
--- src/pages/registered.jsx
+++ src/pages/registered.jsx
@@ -1,24 +1,24 @@
 const Registered = () => {
 	return (
 		<div
-			className="mt-[500px] flex h-32 w-full items-center justify-center gap-x-24 bg-cosBlue sm:gap-x-28
-				lg:my-40 lg:mt-[20%] lg:gap-48"
+			className="mt-[500px] flex h-32 w-full items-center justify-center gap-x-24 bg-cosBlue lg:my-40
+				lg:mt-[20%] lg:gap-48"
 		>
-			<div className="flex sm:flex-col sm:items-center lg:flex-row lg:gap-3">
-				<div className="rounded-full bg-cosWhite text-center sm:h-12 sm:w-12 lg:h-16 lg:w-16">
-					<i className="ri-graduation-cap-line sm:text-[35px] lg:text-[44px]"></i>
+			<div className="flex flex-col items-center lg:flex-row lg:gap-3">
+				<div className="h-12 w-12 rounded-full bg-cosWhite text-center lg:h-16 lg:w-16">
+					<i className="ri-graduation-cap-line text-[35px] lg:text-[44px]"></i>
 				</div>
-				<div className="text-cosWhite sm:text-center lg:text-start">
+				<div className="text-center text-cosWhite lg:text-start">
 					<p>582</p>
 					<p>Registered School</p>
 				</div>
 			</div>
 
-			<div className="flex sm:flex-col sm:items-center lg:flex-row lg:gap-3">
-				<div className="grid rounded-full bg-cosWhite text-center sm:h-12 sm:w-12 lg:h-16 lg:w-16">
-					<i className="ri-user-line sm:text-[36px] lg:text-[44px]"></i>
+			<div className="flex flex-col items-center lg:flex-row lg:gap-3">
+				<div className="grid h-12 w-12 rounded-full bg-cosWhite text-center lg:h-16 lg:w-16">
+					<i className="ri-user-line text-[36px] lg:text-[44px]"></i>
 				</div>
-				<div className="text-cosWhite sm:text-center lg:text-start">
+				<div className="text-center text-cosWhite lg:text-start">
 					<p>98,679</p>
 					<p>Registered Candidates</p>
 				</div>
