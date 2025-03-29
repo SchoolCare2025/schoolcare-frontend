f1b216b37d9681631bf77bd1e645595541f83eba
Ryan-Zayne <phoenixastro97@gmail.com>
2025-03-29 20:39:42 +0100

    .

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "remixicon/fonts/remixicon.css";
-import "./tailwind.css";
+import "../tailwind.css";
 
 createRoot(document.querySelector("#root") as HTMLElement).render(
 	<React.StrictMode>
