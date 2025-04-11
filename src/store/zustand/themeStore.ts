c3742f27494b552a9c5a4fb576eb1c2646969598
Ryan-Zayne <phoenixastro97@gmail.com>
2025-04-11 22:59:27 +0100

    Add API client, theme store, and configure proxy in vercel.json

--- src/store/zustand/themeStore.ts
+++ src/store/zustand/themeStore.ts
@@ -1,93 +1,72 @@
-import { isBrowser, on } from "@zayne-labs/toolkit-core";
-import { isObject } from "@zayne-labs/toolkit-type-helpers";
+import { isBrowser } from "@zayne-labs/toolkit-core";
 import { type StateCreator, create } from "zustand";
 import { persist } from "zustand/middleware";
 
 type ThemeStore = {
 	actions: {
 		initThemeOnLoad: () => void;
+		setTheme: (newTheme: "dark" | "light") => void;
 		toggleTheme: () => void;
 	};
 	isDarkMode: boolean;
+
+	systemTheme: "dark" | "light";
 
 	theme: "dark" | "light" | "system";
 };
 
 const prefersDarkMode = isBrowser() && globalThis.matchMedia("(prefers-color-scheme: dark)").matches;
 
-const systemTheme = prefersDarkMode ? "dark" : "light";
-
-// Store Object Initializtion
+// Store Object Initialization
 const themeStoreObjectFn: StateCreator<ThemeStore> = (set, get) => ({
-	isDarkMode: systemTheme === "dark",
-
 	/* eslint-disable perfectionist/sort-objects */
-	theme: systemTheme,
+
+	theme: "system",
+
+	systemTheme: prefersDarkMode ? "dark" : "light",
+
+	isDarkMode: prefersDarkMode,
 
 	actions: {
+		/* eslint-enable perfectionist/sort-objects */
+
 		initThemeOnLoad: () => {
-			const { theme: persistedTheme } = get();
+			const { systemTheme, theme: persistedTheme } = get();
 
-			if (persistedTheme === "system") {
-				document.documentElement.dataset.theme = systemTheme;
-				return;
-			}
-
-			document.documentElement.dataset.theme = persistedTheme;
+			document.documentElement.dataset.theme =
+				persistedTheme === "system" ? systemTheme : persistedTheme;
 		},
 
-		toggleTheme: () => {
-			const { theme: persistedTheme } = get();
-
-			if (persistedTheme === "system") {
-				document.documentElement.dataset.theme = systemTheme;
-
-				on("transitionend", document.documentElement, () => {
-					document.documentElement.removeAttribute("class");
-				});
-
-				set({ isDarkMode: systemTheme === "dark", theme: systemTheme });
-
-				return;
-			}
-
-			const newTheme = persistedTheme === "light" ? "dark" : "light";
-
+		setTheme: (newTheme: "dark" | "light") => {
 			document.documentElement.dataset.theme = newTheme;
-
-			on("transitionend", document.documentElement, () => {
-				document.documentElement.removeAttribute("class");
-			});
 
 			set({ isDarkMode: newTheme === "dark", theme: newTheme });
 		},
+
+		toggleTheme: () => {
+			const {
+				actions: { setTheme },
+				systemTheme,
+				theme: persistedTheme,
+			} = get();
+
+			const currentTheme = persistedTheme === "system" ? systemTheme : persistedTheme;
+
+			const newTheme = currentTheme === "light" ? "dark" : "light";
+
+			setTheme(newTheme);
+		},
 	},
-	/* eslint-enable perfectionist/sort-objects */
 });
-
-const assertState = (state: unknown) => {
-	if (!isObject(state)) {
-		throw new Error("Invalid app state");
-	}
-
-	return state;
-};
 
 // Store hook Creation
 export const useThemeStore = create<ThemeStore>()(
 	persist(themeStoreObjectFn, {
-		migrate: (persistedState) => {
-			const validPersistedState = assertState(persistedState);
-
-			return validPersistedState;
-		},
+		migrate: (persistedState) => persistedState,
 
 		name: "colorScheme",
 
-		partialize: ({ theme }) => ({ theme }),
+		partialize: ({ isDarkMode, theme }) => ({ isDarkMode, theme }),
 		version: 1,
 	})
 );
-
-// Actions hook
-export const useThemeActions = () => useThemeStore((state) => state.actions);
